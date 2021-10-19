feec9dd14afc4aa2518fe3f9fd8f76ba8180b191
Nikolai Makarevich <nikoladze529@mail.ru>
2021-10-19 14:17:51 +0300

    fix: fix some clock class problems

--- momentum/src/js/clock.js
+++ momentum/src/js/clock.js
@@ -4,7 +4,10 @@ export default class Clock {
   constructor() {
     this._container = null;
     this._username = '';
-    this.render()
+    this._locale = 'en-US';
+    this.render();
+    this.updateClock = this.updateClock.bind(this)
+    this.inputUsername = this.inputUsername.bind(this)
   }
 
   get elem() {
@@ -19,13 +22,18 @@ export default class Clock {
     return this._container.querySelector('input');
   }
 
+  set locale(value) {
+    this._locale = value;
+  }
+
   render() {
     this._container = createElement('clock-container', this.clockTemplate(this.username));
     this.input.addEventListener('change', this.inputUsername);
   }
   
-  inputUsername = () => {
+  inputUsername() {
     this._username = this.value;
+    console.log(this.value)
     localStorage.setItem('momentumUsername', this._username)
   }
 
@@ -33,12 +41,12 @@ export default class Clock {
     return `<div class="time"></div>
     <div class="date"></div>
     <div class="greetings">
-      <p class="greeting"><p>
+      <p class="greeting"></p>
       <input type="text" name="username" id="momentumUsername" placeholder="Enter your name" value="${name}">
     </div>`
   }
 
-  updateClock = (locale='ru-RU') => {
+  updateClock() {
     const dateObj = new Date();
     const time = this._container.querySelector('.time');
     const date = this._container.querySelector('.date');
@@ -46,16 +54,16 @@ export default class Clock {
     const greeting = greetings.querySelector('.greeting');
     const options = {weekday: 'long', month: 'long', day: 'numeric'}
     const partOfDay = {
-      'ru-RU': ['Доброе утро', 'Добрый день', 'Добрый вечер', 'Доброй ночи'],
-      'en-US': ['Good morning', 'Good day', 'Good evening', 'Good night']
+      'ru-RU': ['Доброе утро,&nbsp;', 'Добрый день,&nbsp;', 'Добрый вечер,&nbsp;', 'Доброй ночи,&nbsp;'],
+      'en-US': ['Good morning,&nbsp;', 'Good day,&nbsp;', 'Good evening,&nbsp;', 'Good night,&nbsp;']
     }
 
     const hour = dateObj.getHours();
-    time.innerHTML = dateObj.toLocaleTimeString(locale);
-    date.innerHTML = dateObj.toLocaleDateString(locale, options);
-    greeting.innerHTML = hour >= 0 && hour < 6 ? partOfDay[locale][3] 
-      : hour >= 6 && hour < 12 ? partOfDay[locale][0] 
-      : hour >= 12 && hour < 18 ? partOfDay[locale][1] 
-      : partOfDay[locale][2]
+    time.innerHTML = dateObj.toLocaleTimeString(this._locale);
+    date.innerHTML = dateObj.toLocaleDateString(this._locale, options);
+    greeting.innerHTML = hour >= 0 && hour < 6 ? partOfDay[this._locale][3] 
+      : hour >= 6 && hour < 12 ? partOfDay[this._locale][0] 
+      : hour >= 12 && hour < 18 ? partOfDay[this._locale][1] 
+      : partOfDay[this._locale][2]
   }
 }
