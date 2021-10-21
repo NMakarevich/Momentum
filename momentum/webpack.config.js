70983cbff6362b89e95e86bc95924cc1af5fb00b
Nikolai Makarevich <nikoladze529@mail.ru>
2021-10-21 23:58:08 +0300

    refactor: add webpack.config and package to stage-files

---
+++ momentum/webpack.config.js
@@ -0,0 +1,86 @@
+const path = require('path');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const HTMLWebpackPlugin = require('html-webpack-plugin');
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+const CopyPlugin = require("copy-webpack-plugin");
+
+module.exports = {
+  entry: './src/index.js',
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'index.js',
+    assetModuleFilename: 'assets/'
+  },
+  mode: 'development',
+  devtool: 'inline-source-map',
+  optimization: {
+    minimize: false
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(?:gif|png|jpg|jpeg)$/i,
+        type: 'asset/resource',
+        generator: {
+          filename: 'assets/img/[name].[contenthash][ext]'
+        }
+      },
+      {
+        test: /\.svg$/i,
+        type: 'asset/resource',
+        generator: {
+          filename: 'assets/svg/[name][ext]'
+        }
+      },
+      {
+        test: /\.(woff(2)?|eot|ttf|otf)$/i,
+        type: 'asset/resource',
+        generator: {
+          filename: 'assets/fonts/[name][ext]'
+        }
+      },
+      {
+        test: /\.css$/i,
+        use: [MiniCssExtractPlugin.loader, 'css-loader']
+      },
+      {
+        test: /\.s[ac]ss$/i,
+        use: [MiniCssExtractPlugin.loader, 'css-loader', 'sass-loader']
+      },
+      {
+        test: /\.html$/i,
+        use: ['html-loader']
+      }
+    ]
+  },
+  plugins: [
+    new HTMLWebpackPlugin({
+      template: './src/index.html',
+    }),
+    new MiniCssExtractPlugin({
+      filename: 'style.css',
+    }),
+    new CleanWebpackPlugin({
+      cleanStaleWebpackAssets: false
+    }),
+    new CopyPlugin({
+      patterns: [
+        {
+          from: './src/assets/svg',
+          to: './assets/svg',
+          noErrorOnMissing: true,
+        },
+        {
+          from: './src/assets/playlist',
+          to: './assets/playlist',
+          noErrorOnMissing: true,
+        },
+        {
+          from: './src/assets/fonts/weather',
+          to: './assets/fonts',
+          noErrorOnMissing: true
+        }
+      ]
+    }),
+  ]
+};
