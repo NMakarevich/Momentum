4416fc53d6add8d5c35ac1996594dff79b7e488d
Nikolai Makarevich <nikoladze529@mail.ru>
2021-10-22 22:14:50 +0300

    feat: finish slider class

--- momentum/src/js/slider.js
+++ momentum/src/js/slider.js
@@ -4,6 +4,7 @@ export default class Slider {
   constructor() {
     this._container = null;
     this.number = null;
+    this._imageSource = localStorage.getItem('momentumImageSource') || 'git';
     this.render();
   }
 
@@ -11,12 +12,17 @@ export default class Slider {
     document.body.style.backgroundColor = '#B3B3B3'
     this._container = createElement('slider-buttons', this.sliderTemplate());
     this.number = this.getRandomNum();
-    this.setBackground(this.getTimeOfDay(), this.number);
+    this.setBg(this.getTimeOfDay());
     this._container.addEventListener('click', this.changeBackground);
   }
 
   get elem() {
     return this._container;
+  }
+
+  set imageSource(value) {
+    this._imageSource = value;
+    localStorage.setItem('momentumImageSource', this._imageSource)
   }
 
   getTimeOfDay() {
@@ -36,20 +42,50 @@ export default class Slider {
 
     if(target.classList.contains('button-prev')) {
       this.number === 1 ? this.number = 20 : this.number--;
-      this.setBackground(this.getTimeOfDay(), this.number)
+      this.setBg(this.getTimeOfDay())
     }
     else {
       this.number === 20 ? this.number = 1 : this.number++;
-      this.setBackground(this.getTimeOfDay(), this.number);
+      this.setBg(this.getTimeOfDay());
     }
   }
 
-  setBackground(time, number) {
+  async setBg(time) {
+    if(this._imageSource == 'git') this.setBgGit(time);
+    if(this._imageSource == 'unsplash') this.setBgUnsplash(time);
+    if(this._imageSource == 'flickr') this.setBgFlickr(time);
+  }
+
+  setBgGit(time) {
     const img = new Image();
-    number = number < 10 ? `0${number}` : number;
+    const number = this.number < 10 ? `0${this.number}` : this.number;
     img.src = `https://raw.githubusercontent.com/NMakarevich/stage1-tasks/assets/images/${time}/${number}.jpg`;
     img.onload = () => {
       document.body.style.backgroundImage = `url(${img.src})`;
+    }
+  }
+
+  async setBgUnsplash(time) {
+    const res = await fetch(`https://api.unsplash.com/photos/random/?client_id=iOl0nDotHygXF4pVzFBy3sEQB0V3pkCBbZF8pblRz8Y&orientation=landscape&query=${time},nature`)
+    const body = await res.json();
+    const regular = await body.urls.regular;
+    const img = new Image();
+    img.src = regular;
+    img.onload = () => {
+        document.body.style.backgroundImage = `url(${regular})`;
+    }
+  }
+
+  async setBgFlickr(time) {
+    const res = await fetch(`https://www.flickr.com/services/rest/?method=flickr.photos.search&api_key=26e8f99f024428ff261410fb77ad5110&tags=nature,${time}&tags_mode=all&extras=url_l&format=json&nojsoncallback=1`)
+    const body = await res.json();
+    const photos = await body.photos.photo;
+    const randomPhoto = Math.floor(Math.random() * 100);
+    const photo = await photos[randomPhoto].url_l;
+    const img = new Image();
+    img.src = photo;
+    img.onload = () => {
+        document.body.style.backgroundImage = `url(${photo})`;
     }
   }
 
